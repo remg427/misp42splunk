b851ccba5a21bcf52acb54a96c4a46f9aa3e1544
Remi Seguy <remg427@gmail.com>
2018-11-04 22:46:34 +0100

    prototype using MISP API only

--- misp42splunk/bin/mispioc.py
+++ misp42splunk/bin/mispioc.py
@@ -16,7 +16,7 @@ import sys
 import ConfigParser
 import requests
 import json
-from itertools import chain
+
 from splunklib.searchcommands import dispatch, ReportingCommand, Configuration, Option, validators
 import logging
 
@@ -69,15 +69,60 @@ class mispioc(ReportingCommand):
         require=False, validate=validators.Match("verify_cert", r"^[yYnN01]$"))
 
 
-    eventid         = Option(require=False, validate=validators.Match("eventid",     r"^[0-9]+$"))
-    last            = Option(require=False, validate=validators.Match("last",        r"^[0-9]+[hdm]$"))
-    onlyids         = Option(require=False, validate=validators.Match("onlyids",     r"^[yYnN01]+$"))
-    getuuid         = Option(require=False, validate=validators.Match("getuuid",     r"^[yYnN01]+$"))
-    getorg          = Option(require=False, validate=validators.Match("getorg",      r"^[yYnN01]+$"))
-    category        = Option(require=False)
-    type            = Option(require=False)
-    tags            = Option(require=False)
-    not_tags        = Option(require=False)
+    eventid         = Option(
+        doc = '''
+        **Syntax:** **eventid=***id1(,id2,...)*
+        **Description:**list of event ID(s). **eventid** and **last** are mutually exclusive''',
+        require=False, validate=validators.Match("eventid",     r"^[0-9,]+$"))
+
+    last            = Option(
+        doc = '''
+        **Syntax:** **last=***<int>d|h|m*
+        **Description:**publication duration in day(s), hour(s) or minute(s). **eventid** and **last** are mutually exclusive''',
+        require=False, validate=validators.Match("last",        r"^[0-9]+[hdm]$"))
+
+    onlyids         = Option(
+        doc = '''
+        **Syntax:** **onlyids=***y|Y|n|N|0|1*
+        **Description:**Boolean to search only attributes with the flag "to_ids" set to true.''',
+        require=False, validate=validators.Match("onlyids",     r"^[yYnN01]+$"))
+
+    category        = Option(
+        doc = '''
+        **Syntax:** **category=***CSV string*
+        **Description:**Comma(,)-separated string of categories to search for. Wildcard is %.''',
+        require=False)
+    type            = Option(
+        doc = '''
+        **Syntax:** **type=***CSV string*
+        **Description:**Comma(,)-separated string of categories to search for. Wildcard is %.''',
+        require=False)
+    tags            = Option(
+        doc = '''
+        **Syntax:** **tags=***CSV string*
+        **Description:**Comma(,)-separated string of tags to search for. Wildcard is %.''',
+        require=False)
+    not_tags        = Option(
+        doc = '''
+        **Syntax:** **not_tags=***CSV string*
+        **Description:**Comma(,)-separated string of tags to exclude from results. Wildcard is %.''',
+        require=False)
+
+    getuuid         = Option(
+        doc = '''
+        **Syntax:** **getuuid=***y|Y|n|N|0|1*
+        **Description:**Boolean to return attribute UUID.''',
+        require=False, validate=validators.Match("getuuid",     r"^[yYnN01]+$"))
+    getorg          = Option(
+        doc = '''
+        **Syntax:** **getorg=***y|Y|n|N|0|1*
+        **Description:**Boolean to return the ID of the organisation that created the event.''',
+        require=False, validate=validators.Match("getorg",      r"^[yYnN01]+$"))
+    geteventtag     = Option(
+        doc = '''
+        **Syntax:** **geteventtag=***y|Y|n|N|0|1*
+        **Description:**Boolean to return also event tag(s). By default only attribute tag(s) are returned.''',
+        require=False, validate=validators.Match("geteventtag", r"^[yYnN01]+$"))
 
     @Configuration()
     def map(self, records):
@@ -137,12 +182,13 @@ class mispioc(ReportingCommand):
             logging.error('Options "eventid" and "last" are mutually exclusive')
             raise Exception('Options "eventid" and "last" are mutually exclusive')
         elif self.eventid:
-            body_dict['eventid'] = self.eventid
-            my_args['mispsrv'] = my_args['mispsrv'] + '/events/restSearch'
+            event_criteria = {}
+            event_list = self.eventid.split(",")
+            event_criteria['OR'] = event_list
+            body_dict['eventid'] = event_criteria
             logging.info('Option "eventid" set with %s', body_dict['eventid'])
         elif self.last:
             body_dict['last'] = self.last
-            my_args['mispsrv'] = my_args['mispsrv'] + '/attributes/restSearch'
             logging.info('Option "last" set with %s', body_dict['last'])
         else:
             logging.error('Missing "eventid" or "last" argument')
@@ -157,48 +203,99 @@ class mispioc(ReportingCommand):
         #Search parameters: boolean and filter
         if self.onlyids == 'Y' or self.onlyids == 'y' or self.onlyids == '1':
             body_dict['to_ids'] = True
+        if self.geteventtag == 'Y' or self.geteventtag == 'y' or self.geteventtag == '1':
+            body_dict['includeEventTags'] = True
         if self.category is not None:
-            my_args['category'] = self.category
-        else:
-            my_args['category'] = None
+            cat_criteria = {}
+            cat_list = self.category.split(",")
+            cat_criteria['OR'] = cat_list
+            body_dict['category'] = cat_criteria
         if self.type is not None:
-            my_args['type'] = self.type
+            type_criteria = {}
+            type_list = self.type.split(",")
+            type_criteria['OR'] = type_list
+            body_dict['type'] = type_criteria
+        if self.tags is not None or self.not_tags is not None:
+            tags_criteria = {}
+            if self.tags is not None:
+                tags_list = self.tags.split(",")
+                tags_criteria['OR'] = tags_list
+            if self.not_tags is not None:
+                tags_list = self.not_tags.split(",")
+                tags_criteria['NOT'] = tags_list
+            body_dict['tags'] = tags_criteria
+
+        # output filter parameters
+        if self.getuuid == 'Y' or self.getuuid == 'y' or self.getuuid == '1':
+            my_args['getuuid'] = True
         else:
-            my_args['type'] = None
-        if self.tags is not None:
-            my_args['tags'] = self.tags
+            my_args['getuuid'] = False
+        if self.getorg == 'Y' or self.getorg == 'y' or self.getorg == '1':
+            my_args['getorg'] = True
         else:
-            my_args['tags'] = None
-        if self.not_tags is not None:
-            my_args['not_tags'] = self.not_tags
-        else:
-            my_args['not_tags'] = None
-        
+            my_args['getorg'] = False
+
+        results = []
         while other_page:
             body_dict['page'] = page
             body = json.dumps(body_dict)
+            logging.error('DEBUG MISP REST API REQUEST: %s', body)
             # search 
-            r = requests.post(my_args['mispsrv'], headers=headers, data=body, verify=my_args['verify_cert'])
+            my_args['mispurl'] = my_args['mispsrv'] + '/attributes/restSearch'
+            r = requests.post(my_args['mispurl'], headers=headers, data=body, verify=my_args['verify_cert'])
             # check if status is anything other than 200; throw an exception if it is
             r.raise_for_status()
             # response is 200 by this point or we would have thrown an exception
-            # print >> sys.stderr, "DEBUG MISP REST API response: %s" % response.json()
             response = r.json()
             if 'response' in response:
                 if 'Attribute' in response['response']:
                     l = len(response['response']['Attribute'])
                     for a in response['response']['Attribute']:
                         v = {}
-                        v["event_id"] = str(a["event_id"])
-                        v["category"] = str(a["category"])
-                        v["type"] = str(a["type"])
-                        v["to_ids"] = str(a["to_ids"])
-                        v["value"] = str(a["value"])
-                        yield v
+                        v['category'] = str(a['category'])
+                        v['event_id'] = str(a['event_id'])
+                        v['object_id'] = str(a['object_id'])
+                        v['type'] = str(a['type'])
+                        v['timestamp'] = str(a['timestamp'])
+                        v['to_ids'] = str(a['to_ids'])
+                        v['value'] = str(a['value'])
+                        # v['json'] = json.dumps(a)
+                        # list tag(s) if any in CSV format
+                        tag_delims = ''
+                        tag_string = ''
+                        if 'Tag' in a:
+                            for tag in a['Tag']:
+                                tag_string = tag_string + tag_delims + tag['name']
+                                tag_delims = ','
+                        v['tag'] = tag_string
+                        # include attribute UUID if requested
+                        if my_args['getuuid']:
+                            v['uuid'] = str(a['uuid'])
+                        # include ID of the organisation that created the attribute if requested
+                        # in previous version this was the ORG name ==> create lookup
+                        if 'Event' in a and my_args['getorg']:
+                            v['orgc_id'] = str(a['Event']['orgc_id'])
+                        results.append(v)
+
             if l < limit:
                 other_page = False
             else:
                 page = page + 1
+
+        # add colums for each type in results
+        typelist = []
+        for r in results:
+            if r['type'] not in typelist:
+                typelist.append(r['type'])
+
+        for r in results:
+            v = r
+            for t in typelist:
+                if t == r['type']:
+                    v[t] = r['value']
+                else:
+                    v[t] = ''
+            yield v
 
 
 if __name__ == "__main__":
