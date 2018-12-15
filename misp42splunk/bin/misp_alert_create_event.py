3b9984047c144f817788095b5916e0fd61a49c6b
Remi Seguy <remg427@gmail.com>
2018-12-15 09:25:06 +0100

    v2.0.7 create events using MISP API

--- misp42splunk/bin/misp_alert_create_event.py
+++ misp42splunk/bin/misp_alert_create_event.py
@@ -10,14 +10,15 @@
 # most of the code here was based on the following example on splunk custom alert actions
 # http://docs.splunk.com/Documentation/Splunk/6.5.3/AdvancedDev/ModAlertsAdvancedExample
 
-import os
-import sys
-import subprocess
-import json
-import tempfile
 import ConfigParser
+import csv
+import datetime
+import gzip
+import json
+import os
+import requests
+import sys
 import time
-import pickle
 
 __author__     = "Remi Seguy"
 __license__    = "LGPLv3"
@@ -26,14 +27,216 @@ __maintainer__ = "Remi Seguy"
 __email__      = "remg427@gmail.com"
 
 
-def create_alert(config, filename):
+def store_attribute(t, v, to_ids=False, category=None):
+    Attribute = {}
+    Attribute['type'] = t
+    Attribute['value'] = v
+    Attribute['to_ids'] = to_ids
+    Attribute['category'] = category
+    return Attribute
+
+
+def store_object_attribute(ot, t, v):
+    try:
+        # open object definition.json
+        _SPLUNK_PATH = os.environ['SPLUNK_HOME']
+        # open misp.conf
+        object_definition = _SPLUNK_PATH + os.sep + 'etc' + os.sep + 'apps' + os.sep + 'misp42splunk' + os.sep + 'bin' + os.sep + ot + '_definition.json'
+        with open(object_definition) as json_object:
+            od = json.load(json_object)
+            object_attributes = od['attributes']
+            Attribute = {}
+            if t in object_attributes:
+                Attribute['type'] = object_attributes[t]['misp-attribute']
+                Attribute['object_relation'] =  t
+                Attribute['value'] = v
+    
+        return Attribute
+    except IOError as e:
+        print("FATAL %s object definition could not be opened/read" % ot)
+        exit(3)    
+
+
+def prepare_misp_events(config, results):
+    # print(config)
+    # iterate through each row, cleaning multivalue fields and then adding the attributes under same event key
+    # this builds the dict events
+    events = {}
+    event_baseline = {
+        'threat_level_id': config['threatlevel'],
+        'analysis': config['analysis'],
+        'distribution': config['distribution'],
+        'published': False,
+        'Attribute': [],
+        'Object': [],
+
+    }
+    # tag the event with TLP level
+    tags = [{ 'name': config['tlp']}]
+    # Add tags set in alert definition
+    if config['tags'] is not None:
+        tag_list = config['tags'].split(',')
+        for tag in tag_list:
+            if tag not in tags:
+                new_tag = { 'name': tag }
+                tags.append(new_tag)
+    event_baseline['Tag'] = tags
+
+    for row in results:
+        # Splunk makes a bunch of dumb empty multivalue fields - we filter those out here
+        row = {key: value for key, value in row.items() if not key.startswith("__mv_")}
+
+        # GEt the specific eventkey if define in Splunk search. Defaults to alert form got above
+        eventkey = config['eventkey']
+        if eventkey in row:
+            eventkey = row.pop(eventkey)
+
+        # check if building event has been initiated
+        # if yes simply add attribute entry otherwise collect other metadata
+        # remove fields misp_time and info from row and keep their values if this is a new event
+        if eventkey in events:
+            event = events[eventkey]
+            attributes = event['Attribute']
+            objects = event['Object']
+            tags = event['Tag']
+            if 'misp_time' in row:
+                row.pop('misp_time')
+            if 'misp_info' in row:
+                row.pop('misp_info')
+        else:
+            event = event_baseline
+            attributes = event['Attribute']
+            objects = event['Object']
+            tags = event['Tag']
+            if 'misp_time' in row:
+                event['date'] = datetime.datetime.fromtimestamp(int(row.pop('misp_time'))).strftime('%Y-%m-%d')
+            else:
+                event['date'] = datetime.datetime.fromtimestamp(int(time.time())).strftime('%Y-%m-%d')
+            if 'misp_info' in row:
+                event['info'] = row.pop('misp_info')
+            else:
+                event['info'] = config['misp_info']
+
+        # append event tags provided in the row
+        if 'misp_tag' in row:
+            tag_list = row.pop('misp_tag').split(',')
+            for tag in tag_list:
+                if tag not in tags:
+                    new_tag = { 'name': tag }
+                    tags.append(new_tag)
+
+        # update event tag list
+        event['Tag'] = tags
+
+        # collect attribute value and build type=value entry
+        if 'misp_to_ids' in row:
+            if str(row.pop('misp_to_ids')) == 'True':
+                to_ids = True
+            else:
+                to_ids = False
+        else:
+            to_ids = False
+
+        if 'misp_category' in row:
+            category = str(row.pop('misp_category'))
+        else:
+            category = None
+
+        # now we take KV pairs starting by misp_ to add to event as single attribute(s)
+        for key, value in row.items():
+            if key.startswith("misp_") and value != "":
+                misp_key = str(key).replace('misp_', '').replace('_', '-')
+                attributes.append(store_attribute(misp_key, str(value), to_ids, category))
+
+        # update event attribute list
+        event['Attribute'] = attributes
+
+        # now we look for attribute belonging to a file email or network object i.e.
+        # on the same row, field(s) start(s) with fo_, eo_ or no_
+        fo_attribute = []
+        eo_attribute = []
+        no_attribute = []
+        for key, value in row.items():
+            if key.startswith("fo_") and value != "":
+                fo_key = str(key).replace('fo_', '').replace('_', '-')
+                object_attribute = store_object_attribute('file',fo_key, str(value))
+                if object_attribute:
+                    fo_attribute.append(object_attribute)
+            if key.startswith("eo_") and value != "":
+                eo_key = str(key).replace('eo_', '').replace('_', '-')
+                object_attribute = store_object_attribute('email',eo_key, str(value))
+                if object_attribute:
+                    eo_attribute.append(object_attribute)
+            if key.startswith("no_") and value != "":
+                no_key = str(key).replace('no_', '').replace('_', '-')
+                object_attribute = store_object_attribute('domain-ip',no_key, str(value))
+                if object_attribute:
+                    no_attribute.append(object_attribute)
+
+        if fo_attribute:
+            new_object = {
+                'name': 'file',
+                'Attribute': fo_attribute
+            }
+            objects.append(new_object)
+
+        if eo_attribute:
+            new_object = {
+                'name': 'email',
+                'Attribute': eo_attribute
+            }
+            objects.append(new_object)
+
+        if no_attribute:
+            new_object = {
+                'name': 'domain-ip',
+                'Attribute': no_attribute
+            }
+            objects.append(new_object)
+        # update event object list
+        event['Object'] = objects
+
+        # update event defintion
+        events[eventkey] = event
+
+    # events are prepared; now return them
+    return events
+
+
+def create_misp_events(config, results):
+
+    #create them in MISP
+    # print(events)
+
+    misp_url = config['misp_url'] + '/events/add'
+    misp_key = config['misp_key']
+    misp_verifycert = config['misp_verifycert']
+
+    # set proper headers
+    headers = {'Content-type': 'application/json'}
+    headers['Authorization'] = misp_key
+    headers['Accept'] = 'application/json'
+
+    status = 200
+    for eventkey in results:
+        body = json.dumps(results[eventkey])
+        # POST json data to create events
+        r = requests.post(misp_url, headers=headers, data=body, verify=misp_verifycert)
+        # check if status is anything other than 200; throw an exception if it is
+        r.raise_for_status()
+        # response is 200 by this point or we would have thrown an exception
+        response = r.json()
+        # print(json.dumps(response))
+
+    return status
+
+def prepare_config(config, filename):
     print >> sys.stderr, "DEBUG Creating alert with config %s" % json.dumps(config)
 
     # get the misp_url we need to connect to MISP
     # this can be passed as params of the alert. Defaults to values set in misp.conf
     # get MISP settings stored in misp.conf
     _SPLUNK_PATH = os.environ['SPLUNK_HOME']
-
     # open misp.conf
     config_file = _SPLUNK_PATH + os.sep + 'etc' + os.sep + 'apps' + os.sep + 'misp42splunk' + os.sep + 'local' + os.sep + 'misp.conf'
     mispconf = ConfigParser.RawConfigParser()
@@ -80,42 +283,7 @@ def create_alert(config, filename):
     # add filename of the file containing the result of the search
     config_args['filename'] = filename
 
-    try:
-
-        # path to main components either use default values or set ones
-        if mispconf.has_option('mispsetup', 'P3_PATH'):
-            _NEW_PYTHON_PATH = mispconf.get('mispsetup', 'P3_PATH')
-        else:
-            _NEW_PYTHON_PATH = '/usr/bin/python3'
-
-        os.environ['PYTHONPATH'] = _NEW_PYTHON_PATH
-
-        env = dict(os.environ)
-        del env['LD_LIBRARY_PATH']
-
-        my_process = _SPLUNK_PATH + os.sep + 'etc' + os.sep + 'apps' + os.sep + 'misp42splunk' + os.sep + 'bin' + os.sep + 'pymisp_create_event.py'
-        # Remove LD_LIBRARY_PATH from the environment (otherwise, we will face some SSL issues
-
-        # use pickle
-        _TMP_PATH = tempfile.mkdtemp()
-        config_file = _TMP_PATH + '/misp42_alert_create'
-        pickle.dump(config_args, open(config_file, "wb"), protocol=2)
-
-        print >> sys.stderr, "DEBUG env: %s" % env
-        try:
-            p = subprocess.Popen([_NEW_PYTHON_PATH, my_process, config_file],
-                             stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
-            output, err = p.communicate()
-            rc = p.returncode
-            print >> sys.stderr, "INFO returncode %s" % str(rc)
-            if rc != 0:
-                print >> sys.stderr, "error in pymisp_create_event.py. error_code %s" % str(rc)
-        except Exception:
-            print >> sys.stderr, "subprocess failed"
-    # somehow we got a bad response code from thehive
-    # some other request error occurred
-    except IOError as e:
-        print >> sys.stderr, "preparing call of pymisp_create_event.py: %s" % e
+    return config_args
 
 
 if __name__ == "__main__":
@@ -125,20 +293,28 @@ if __name__ == "__main__":
         payload = json.loads(sys.stdin.read())
         # extract the file path and alert config from the payload
         configuration = payload.get('configuration')
-        filepath = payload.get('results_file')
+        filename = payload.get('results_file')
 
         # test if the results file exists - this should basically never fail unless we are parsing configuration incorrectly
         # example path this variable should hold: '/opt/splunk/var/run/splunk/12938718293123.121/results.csv.gz'
-        if os.path.exists(filepath):
+        if os.path.exists(filename):
             # file exists - try to open and if successful add path to configuration
             try:
                 # open the file with gzip lib, start making alerts
                 # can with statements fail gracefully??
-                # configuration['filepath'] = filepath
-                # DictReader lets us grab the first row as a header row and other lines will read as a dict mapping the header to the value
-                # instead of reading the first line with a regular csv reader and zipping the dict manually later
-                # at least, in theory
-                create_alert(configuration, filepath)
+                with gzip.open(filename, 'rt') as file:
+                    # DictReader lets us grab the first row as a header row and
+                    # other lines will read as a dict mapping the header
+                    # to the value instead of reading the first line with a
+                    # regular csv reader and zipping the dict manually later at
+                    # least, in theory
+                    Reader = csv.DictReader(file)
+                    Config = prepare_config(configuration,filename)
+
+                    Events = prepare_misp_events(Config, Reader)
+                    #print(json.dumps(Events))
+                    status = create_misp_events(Config, Events)
+
                 # by this point - all alerts should have been created with all necessary observables attached to each one
                 # we can gracefully exit now
                 sys.exit(0)
