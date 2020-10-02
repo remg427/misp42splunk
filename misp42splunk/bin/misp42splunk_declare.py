3080c86f14cabd057ceb5923877cff199ea3265a
remg427@gmail.com <52327a24e16dd744b09cf9f5f9bdd4d2cd1c3794>
2020-10-02 20:26:54 +0200

    3.2.3

--- misp42splunk/bin/misp42splunk_declare.py
+++ misp42splunk/bin/misp42splunk_declare.py
@@ -7,6 +7,7 @@ This file is genrated by Splunk add-on builder
 """
 
 import os
+import re
 import sys
 
 if sys.version_info[0] < 3:
@@ -14,5 +15,10 @@ if sys.version_info[0] < 3:
 else:
     py_version = "aob_py3"
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib"))
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "..", "lib", py_version))
+ta_name = 'misp42splunk'
+ta_lib_name = 'lib'
+pattern = re.compile(r"[\\/]etc[\\/]apps[\\/][^\\/]+[\\/]bin[\\/]?$")
+new_paths = [path for path in sys.path if not pattern.search(path) or ta_name in path]
+new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name]))
+new_paths.insert(0, os.path.sep.join([os.path.dirname(__file__), ta_lib_name, py_version]))
+sys.path = new_paths
