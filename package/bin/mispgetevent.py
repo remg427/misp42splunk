fcbfc60f906a26439a688b8d040c2567516249dd
remg427@gmail.com <52327a24e16dd744b09cf9f5f9bdd4d2cd1c3794>
2023-03-19 16:16:52 +0100

    fix issue with date=

--- package/bin/mispgetevent.py
+++ package/bin/mispgetevent.py
@@ -314,8 +314,8 @@ class MispGetEventCommand(GeneratingCommand):
         doc='''
         **Syntax:** **date=***The user set event date field
          - any of valid time related filters"*
-        **Description:**starting date. **eventid**, **last**
-         and **date** are mutually exclusive''',
+        **Description:**starting date equivalent to key from.
+        **eventid**, **last** and **date** are mutually exclusive''',
         require=False)
     # Other params
     category = Option(
@@ -509,9 +509,9 @@ class MispGetEventCommand(GeneratingCommand):
             self.log_info('Option "last" set with {}'
                           .format(body_dict['last']))
         else:
-            body_dict['date'] = self.date.split()
+            body_dict['from'] = self.date
             self.log_info('Option "date" set with {}'
-                          .format(json.dumps(body_dict['date'])))
+                          .format(json.dumps(body_dict['from'])))
 
         # Force some values on JSON request
         body_dict['returnFormat'] = 'json'
